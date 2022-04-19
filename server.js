e5912149e8d016c71aec6817685a79a52cff8e90
Kevyn Hotts <kevynh36@gmail.com>
2022-04-18 21:24:17 -0400

    complete project

--- server.js
+++ server.js
@@ -1,5 +1,50 @@
 const express = require('express')
 const app = express()
+const data = require('./data.json')
+
+app.use(express.json()) //falando para o express usar Json
+
+app.get('/clients', function (req, res) {
+  // Antigamente se utilizava XML
+  // hoje se utiliza Json
+  res.json(data)
+})
+
+app.get('/clients/:id', function (req, res) {
+  const { id } = req.params
+  const client = data.find(cli => cli.id == id)
+
+  if (!client) return res.status(204).json()
+
+  res.json(client)
+})
+
+app.post('/clients', function (req, res) {
+  const { name, email } = req.body
+
+  // salvar
+  res.json({ name, email })
+})
+
+app.put('/clients/:id', function (req, res) {
+  const { id } = req.params
+  const client = data.find(cli => cli.id == id)
+
+  if (!client) return res.status(204).json()
+
+  const { name } = req.body
+
+  client.name = name // name é igual ao novo name que foi pego do body
+
+  res.json(client) //responde o cliente atualizado
+})
+
+app.delete('/clients/:id', function (req, res) {
+  const { id } = req.params
+  const clientsFiltered = data.filter(client => client.id != id)
+
+  res.json(clientsFiltered)
+})
 
 app.listen(3000, function () {
   console.log('Server is running')
@@ -13,12 +58,17 @@ app.listen(3000, function () {
   DELETE: Deleta um Resource
 */
 
-app.get('/clients', function (req, res) {
-  // Antigamente se utilizava XML
-  // hoje se utiliza Json
-})
-
-app.get('/clients', function (req, res) {})
-app.post('/clients', function (req, res) {})
-app.put('/clients', function (req, res) {})
-app.delete('/clients', function (req, res) {})
+/*
+  STATUS DAS RESPOSTAS
+  1xx - Informação
+  2xx - Sucesso
+    200: ok 
+    201: CREATED
+    204: Não tem conteúdo PUT POST DELETE
+  3xx - Redirection
+  4xx - Client Error
+    400: Bad Requeset
+    404: Not Found
+  5xx - Server Error
+    500: Internal Server Error
+*/
